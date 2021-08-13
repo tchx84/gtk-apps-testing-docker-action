347b1ecd101ba8a40ca00c72a2659593308ed692
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2021-08-13 19:11:36 -0400

    entrypoint: workaround gnome.compile_resources issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
+
 meson _test && cd _test
+
+# XXX
+(cd ../src; /usr/bin/glib-compile-resources portfolio.gresource.xml; cp portfolio.gresource ../_test/src);
+
 xvfb-run --auto-servernum meson test --verbose
