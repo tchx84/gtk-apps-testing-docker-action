a74b2707a27d4bfaa1f6dccc0b73d758dbcb5f47
Martin Abente Lahaye <martin.abente.lahaye@gmail.com>
2021-09-02 15:41:39 -0400

    entrypoint: Fix Flatseal build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
 meson _test && cd _test
+
+# XXX
+(cd ../src; /usr/bin/glib-compile-resources com.github.tchx84.Flatseal.data.gresource.xml; cp com.github.tchx84.Flatseal.data.gresource ../_test/src);
+(cd ../src; /usr/bin/glib-compile-resources com.github.tchx84.Flatseal.src.gresource.xml; cp com.github.tchx84.Flatseal.src.gresource ../_test/src);
+
 xvfb-run --auto-servernum meson test --verbose
