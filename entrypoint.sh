988ac1ddefec3919e5a60299c3c5a63c8c326f46
Martin Abente Lahaye <martin@sorcero.com>
2022-03-07 08:03:03 -0300

    entrypoint: Properly generate gresource files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-meson _test && cd _test
-
-# XXX
-(cd ../src; /usr/bin/glib-compile-resources com.github.tchx84.Flatseal.data.gresource.xml; cp com.github.tchx84.Flatseal.data.gresource ../_test/src);
-(cd ../src; /usr/bin/glib-compile-resources com.github.tchx84.Flatseal.src.gresource.xml; cp com.github.tchx84.Flatseal.src.gresource ../_test/src);
+meson _test && cd _test && ninja
 
 xvfb-run --auto-servernum meson test --verbose
